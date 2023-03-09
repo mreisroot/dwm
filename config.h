ad9a32e6b2f45c91dc4dd11b9bf59c3926297441
Miguel Reis <miguel_reis08@protonmail.com>
2023-03-09 09:05:29 -0300

    Update configuration

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *termcmd2[]  = { "darktile", NULL };
+static const char *termcmd2[]  = { "urxvt", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
